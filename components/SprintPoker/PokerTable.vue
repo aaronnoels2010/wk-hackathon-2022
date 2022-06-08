21159a3c64bddd7203c35e4aac3dc3ecdefffbc2
WBK\tibeau.schodts <tibeau.schodts@wolterskluwer.com>
2022-06-08 13:05:36 +0200

    Test

--- components/SprintPoker/PokerTable.vue
+++ components/SprintPoker/PokerTable.vue
@@ -5,23 +5,23 @@ import TableSeat from '~/components/SprintPoker/TableSeat'
 <template>
   <div class="flex">
     <div class="flex flex-col items-center justify-center">
-      <TableSeat></TableSeat>
+      <TableSeat />
     </div>
     <div class="flex flex-col">
       <div class="flex justify-between mx-12">
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
+        <TableSeat />
+        <TableSeat />
+        <TableSeat />
       </div>
-      <div class="m-4 min-w-[380px] h-[140px] bg-gray-300 rounded-full"></div>
+      <div class="m-4 min-w-[380px] h-[140px] bg-gray-300 rounded-full" />
       <div class="flex justify-between mx-12">
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
-        <TableSeat></TableSeat>
+        <TableSeat />
+        <TableSeat />
+        <TableSeat />
       </div>
     </div>
     <div class="flex flex-col items-center justify-center">
-      <TableSeat></TableSeat>
+      <TableSeat />
     </div>
   </div>
 </template>
