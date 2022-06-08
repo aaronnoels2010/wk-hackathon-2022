76732c2af134301928355c42301b12e3c72e40e8
WBK\tibeau.schodts <tibeau.schodts@wolterskluwer.com>
2022-06-08 15:57:26 +0200

    Updated poker table

--- components/SprintPoker/TableSeat.vue
+++ components/SprintPoker/TableSeat.vue
@@ -6,14 +6,16 @@ interface Props {
 
 const props = withDefaults(defineProps<Props>(), {
   name: '',
-  score: undefined,
+  score: null,
 })
 </script>
 
 <template>
-  <div>
-    <div class="min-w-[60px] h-[60px] bg-gray-300 rounded-full">
-      <span v-if="score">{{ score }}</span>
+  <div class="flex flex-col items-center">
+    <div :class="{ 'bg-gray-300': score, 'bg-gray-100': !score }" class=" dark:bg-gray-700 w-[60px] h-[60px] rounded-full flex justify-center items-center">
+      <h4 v-if="score" class="text-3xl dark:text-white">
+        {{ score }}
+      </h4>
     </div>
     <span>{{ name }}</span>
   </div>
