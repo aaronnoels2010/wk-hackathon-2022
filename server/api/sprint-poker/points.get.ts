2aec9756464d403dc6e66609ac894b4b9a9254c5
Niels Knapen <niels.knapen@wolterskluwer.com>
2022-06-09 14:29:42 +0200

    remove 0

--- server/api/sprint-poker/points.get.ts
+++ server/api/sprint-poker/points.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(() => {
   return {
-    points: [0, 0.5, 1, 2, 3, 5, 8, 13, 21, 'infinity', 'unknown', 'coffee'],
+    points: [0.5, 1, 2, 3, 5, 8, 13, 21, 'infinity', 'unknown', 'coffee'],
   }
 })
