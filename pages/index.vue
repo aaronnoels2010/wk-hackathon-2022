b55b3b90fcdc5b77c0d6cb0eda007f820cee4d2e
Tibeau Schodts <tibeau.schodts@gmail.com>
2022-10-07 15:58:11 +0200

    Rebranded WittyBoard to Majestic Biscuit

--- pages/index.vue
+++ pages/index.vue
@@ -13,8 +13,11 @@ function goToRetro() {
 <template>
   <div class="py-20 px-10 text-center">
     <Head>
-      <link rel="shortcut icon" href="../public/favicon/favicon.ico">
-      <Title>Home</Title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <link rel="manifest" href="/site.webmanifest">
+      <Title>Majestic Biscuit</Title>
     </Head>
     <div class="flex">
       <div class="m-4">
