c4afbb213ec956f5ac79e417e78a8c216845c6c5
Niels Knapen <niels.knapen@wolterskluwer.com>
2022-10-07 17:21:32 +0200

    Add settings button in header

---
+++ components/SettingsButton.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+import HeroiconsOutlineCog from '~icons/heroicons-outline/cog'
+
+const router = useRouter()
+
+function navigateToSettings() {
+  router.push('/settings')
+}
+</script>
+
+<template>
+  <button
+    class="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200"
+    @click="navigateToSettings()"
+  >
+    <HeroiconsOutlineCog class="w-6 h-6" />
+  </button>
+</template>
